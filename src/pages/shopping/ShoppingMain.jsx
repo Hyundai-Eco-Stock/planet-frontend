e3d10a947e76032e09349a1a403d2ff4dd68f34d
JaeYoungJung <hanol98@naver.com>
2025-08-14 17:19:57 +0900

    feat: 에코딜 상품 목록 가져오는 api 연결

--- src/pages/shopping/ShoppingMain.jsx
+++ src/pages/shopping/ShoppingMain.jsx
@@ -1,8 +1,18 @@
+import { searchTodayAllEcoDealProducts } from "../../api/product/ecoProduct.api";
+
 const ShoppingMain = () => {
+
+    const handleEcoDealProducts = () => {
+        const res = searchTodayAllEcoDealProducts();
+        console.log(res);
+    }
 
     return (
         <div>
             <h2>쇼핑 메인</h2>
+            <button onClick={handleEcoDealProducts}>
+                에코딜 상품 가져오기
+            </button>
         </div>
     );
 }
