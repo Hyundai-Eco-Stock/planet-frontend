81b3ba06262a86af6c73a028fbe906a77132a266
JaeYoungJung <hanol98@naver.com>
2025-08-18 10:36:55 +0900

    feat: 오늘의 에코딜 상품 가져오는 API 개발

--- src/api/product/ecoProduct.api.js
+++ src/api/product/ecoProduct.api.js
@@ -1,6 +1,5 @@
 import apiClient from "@/api/_base/apiClient";
 
-// 로컬 로그인 요청
 export const searchTodayAllEcoDealProducts = async () => {
 	const response = await apiClient.get("/eco-products/today");
 	return response.data;
