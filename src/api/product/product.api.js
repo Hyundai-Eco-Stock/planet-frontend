2d8fc96ad006b51174f8ecc873fd5cb8195e56cc
hyeesw <hyeeun7904@gmail.com>
2025-08-25 09:34:53 +0900

    refactor: param productId 로 변경

--- src/api/product/product.api.js
+++ src/api/product/product.api.js
@@ -1,8 +1,8 @@
 import apiClient from "@/api/_base/apiClient";
 
-export const searchRecommendProducts = async (name, categoryId, id, size) => {
+export const searchRecommendProducts = async ({name, categoryId, productId, size}) => {
   const response = await apiClient.get("/products/recommend", {
-    params: { name, categoryId, id, size },
+    params: { name, categoryId, productId, size },
   });
   console.log(response.data);
   return response.data;
