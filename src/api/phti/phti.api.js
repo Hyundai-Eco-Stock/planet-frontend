f67a644c22414e8f12fa38e216bc6c021d1eac47
JaeYoungJung <hanol98@naver.com>
2025-09-01 11:29:18 +0900

    feat: PHTI 검사 및 결과 나오는 페이지 초기 개발

--- src/api/phti/phti.api.js
+++ src/api/phti/phti.api.js
@@ -4,3 +4,8 @@ export const fetchPhtiQuestinosAndChoices = async () => {
 	const response = await apiClient.get('/phti/questions-with-choices');
 	return response.data;
 }
+
+export const submitPhtiSurvey = async (payload) => {
+	const response = await apiClient.post('/phti/surveys', payload);
+	return response.data;
+}
