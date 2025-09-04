96120dfb078d9489c596cf7a8b50ca988904ec7a
JaeYoungJung <hanol98@naver.com>
2025-09-04 10:13:12 +0900

    style: 장바구니 아이콘과 하단바 쇼핑 아이콘 변경

--- src/components/_layout/Footer.jsx
+++ src/components/_layout/Footer.jsx
@@ -1,6 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 
-import Hamburger from '@/assets/navigation_icon/Hamburger.svg';
+import Shopping from '@/assets/navigation_icon/Shopping.svg';
 import House from '@/assets/navigation_icon/House.svg';
 import My from '@/assets/navigation_icon/My.svg';
 import Planet from '@/assets/navigation_icon/Planet.svg';
@@ -11,7 +11,7 @@ const Footer = () => {
 
     const menus = [
         { name: "홈", icon: House, path: "/home/main" },
-        { name: "쇼핑", icon: Hamburger, path: "/shopping/main" },
+        { name: "쇼핑", icon: Shopping, path: "/shopping/main" },
         { name: "에코스톡", icon: Planet, path: "/eco-stock/main" },
         // { name: "에코딜", icon: Planet, path: "/eco-deal/main" },
         { name: "마이", icon: My, path: "/my-page/main" },
