b16c59276f72eeb6bdea9ba158812e77d8417c37
JaeYoungJung <hanol98@naver.com>
2025-09-08 14:49:19 +0900

    feat: 모바일 포그라운드 알림 모달 구현

--- src/pages/admin/AdminHome.jsx
+++ src/pages/admin/AdminHome.jsx
@@ -1,4 +1,5 @@
 
+import { testNotification } from "@/api/admin/admin.api";
 import { CustomCommonButton } from "@/components/_custom/CustomButtons";
 import { useNavigate } from "react-router-dom";
 
@@ -12,6 +13,7 @@ const AdminHome = () => {
             <CustomCommonButton onClick={() => { navigate("/offline-pay/create") }} children="오프라인 결제 포스기 관리" />
             <CustomCommonButton onClick={() => { navigate("/car-access-history/create") }} children="차량 입출차 시스템 관리" />
             <CustomCommonButton onClick={() => { navigate("/admin/dashboard/main") }} children="통계 대시보드" />
+            <CustomCommonButton onClick={() => { testNotification() }} children="알림 테스트" />
         </div>
     )
 }
