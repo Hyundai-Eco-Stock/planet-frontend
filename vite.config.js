956afa153cb5db33091d22c848cb0e33164e416d
JaeYoungJung <hanol98@naver.com>
2025-09-11 17:09:42 +0900

    fix: 자동 생성된 이미지가 아닌 수동으로 넣은 이미지로 스플래시 이미지 뜨도록 수정

--- vite.config.js
+++ vite.config.js
@@ -25,26 +25,39 @@ export default defineConfig({
         description: 'Planet PWA Service',
         icons: [
           {
-            src: 'pwa-192x192.png',
+            src: '/android/android-launchericon-48-48.png',
+            sizes: '48x48',
+            type: 'image/png'
+          },
+          {
+            src: '/android/android-launchericon-72-72.png',
+            sizes: '72x72',
+            type: 'image/png'
+          },
+          {
+            src: '/android/android-launchericon-96-96.png',
+            sizes: '96x96',
+            type: 'image/png'
+          },
+          {
+            src: '/android/android-launchericon-144-144.png',
+            sizes: '144x144',
+            type: 'image/png'
+          },
+          {
+            src: '/android/android-launchericon-192-192.png',
             sizes: '192x192',
-            type: 'image/png',
+            type: 'image/png'
           },
           {
-            src: 'pwa-512x512.png',
+            src: '/android/android-launchericon-512-512.png',
             sizes: '512x512',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
-          },
+            type: 'image/png'
+          }
         ],
       },
       pwaAssets: {
-        preset: 'minimal-2023',
-        images: ['public/planet-logo-512.png'],
+        disabled: true
       },
       devOptions: {
         enabled: false,
